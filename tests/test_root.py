fe8d8c3b2e9f1de63c140b3759ab062d248c427c
James Addison <james@reciperadar.com>
2019-11-29 21:13:03 +0000

    Add list of vessels for direction parsing

--- tests/test_root.py
+++ tests/test_root.py
@@ -8,3 +8,9 @@ def test_utensil_query(client):
     response = client.get('/queries', query_string={'type': 'utensils'})
 
     assert 'whisk' in response.json
+
+
+def test_vessel_query(client):
+    response = client.get('/queries', query_string={'type': 'vessels'})
+
+    assert 'dutch oven' in response.json
