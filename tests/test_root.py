24bd0351c65d25bad295bb0b8df07f8c0100d1f6
James Addison <james@reciperadar.com>
2019-11-29 20:57:28 +0000

    Add matching appliance query

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,4 +1,10 @@
-def test_request(client):
+def test_appliance_query(client):
+    response = client.get('/queries', query_string={'type': 'appliances'})
+
+    assert 'blender' in response.json
+
+
+def test_utensil_query(client):
     response = client.get('/queries', query_string={'type': 'utensils'})
 
     assert 'whisk' in response.json
