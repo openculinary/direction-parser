09f25792dc72dae8e4141d4ba542ea6893c1ff7b
James Addison <james@reciperadar.com>
2020-04-29 19:06:56 +0100

    Switch to shared hashedixsearch library

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,16 +1,43 @@
-def test_appliance_query(client):
+def test_appliance_queries(client):
     response = client.get('/queries', query_string={'type': 'appliances'})
 
     assert 'blender' in response.json
 
 
-def test_utensil_query(client):
+def test_utensil_queries(client):
     response = client.get('/queries', query_string={'type': 'utensils'})
 
     assert 'whisk' in response.json
 
 
-def test_vessel_query(client):
+def test_vessel_queries(client):
     response = client.get('/queries', query_string={'type': 'vessels'})
 
     assert 'dutch oven' in response.json
+
+
+def test_description_parsing(client):
+    description_equipment = {
+        'pre-heat the oven to 300 degrees': {
+            'appliances': ['oven'],
+        },
+        'leave the slow cooker on a low heat for three hours': {
+            'appliances': ['slow cooker'],
+        },
+        'remove the casserole dish from the oven': {
+            'appliances': ['oven'],
+            'vessels': ['casserole dish'],
+        },
+        'place the skewer in the frying pan': {
+            'utensils': ['skewer'],
+            'vessels': ['frying pan'],
+        },
+    }
+
+    response = client.post('/', data={
+        'descriptions[]': list(description_equipment.keys())
+    })
+    for result in response.json:
+        assert result['description'] in description_equipment
+        for key, value in description_equipment[result['description']].items():
+            assert result[key] == value
--- web/app.py
+++ web/app.py
@@ -1,13 +1,21 @@
+from collections import defaultdict
 from flask import Flask, abort, jsonify, request
-
-from web.search import (
+from hashedixsearch import (
    build_search_index,
    execute_queries,
-   load_queries,
+   tokenize,
 )
+from stop_words import get_stop_words as get_stopwords
+
+
+def load_queries(filename):
+    with open(filename) as f:
+        return [line.strip().lower() for line in f.readlines()]
 
 
 app = Flask(__name__)
+
+stopwords = get_stopwords('en')
 
 appliance_queries = load_queries('web/data/appliances.txt')
 utensil_queries = load_queries('web/data/utensils.txt')
@@ -28,31 +36,36 @@ def queries():
     return jsonify(queries)
 
 
+def equipment_by_document(index, queries):
+    results_by_document = defaultdict(lambda: set())
+    equipment_hits = execute_queries(index, queries)
+    for equipment, hits in equipment_hits:
+        for hit in hits:
+            results_by_document[hit['doc_id']].add(equipment)
+    return results_by_document
+
+
 @app.route('/', methods=['POST'])
 def root():
     descriptions = request.form.getlist('descriptions[]')
 
-    index = build_search_index(descriptions)
-    appliance_hits = execute_queries(index, appliance_queries)
-    utensil_hits = execute_queries(index, utensil_queries)
-    vessel_hits = execute_queries(index, vessel_queries)
+    index = build_search_index()
+    for doc_id, doc in enumerate(descriptions):
+        for ngrams in [1, 2]:
+            for term in tokenize(doc, stopwords=stopwords, ngrams=ngrams):
+                index.add_term_occurrence(term, doc_id)
+
+    appliances_by_doc = equipment_by_document(index, appliance_queries)
+    utensils_by_doc = equipment_by_document(index, utensil_queries)
+    vessels_by_doc = equipment_by_document(index, vessel_queries)
 
     results = []
     for doc_id, description in enumerate(descriptions):
         results.append({
             'index': doc_id,
             'description': description,
-            'appliances': [
-                {'appliance': appliance}
-                for appliance in appliance_hits[doc_id]
-            ],
-            'utensils': [
-                {'utensil': utensil}
-                for utensil in utensil_hits[doc_id]
-            ],
-            'vessels': [
-                {'vessel': vessel}
-                for vessel in vessel_hits[doc_id]
-            ],
+            'appliances': list(appliances_by_doc[doc_id]),
+            'utensils': list(utensils_by_doc[doc_id]),
+            'vessels': list(vessels_by_doc[doc_id]),
         })
     return jsonify(results)
