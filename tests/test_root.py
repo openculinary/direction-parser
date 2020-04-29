ed5ce66c8fa6deccdf83561ed04bb4369e4d9615
James Addison <james@reciperadar.com>
2020-04-29 23:08:59 +0100

    Migrate to use hashedixsearch library (#8)

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,16 +1,47 @@
-def test_appliance_query(client):
+def test_appliance_queries(client):
     response = client.get('/queries', query_string={'type': 'appliances'})
 
     assert 'blender' in response.json
 
 
-def test_utensil_query(client):
+def test_utensil_queries(client):
     response = client.get('/queries', query_string={'type': 'utensils'})
 
     assert 'whisk' in response.json
 
 
-def test_vessel_query(client):
+def test_vessel_queries(client):
     response = client.get('/queries', query_string={'type': 'vessels'})
 
     assert 'dutch oven' in response.json
+
+
+def test_description_parsing(client):
+    description_equipment = {
+        'pre-heat the oven': {
+            'markup': 'preheat the <mark>oven</mark>',
+            'appliances': ['oven'],
+        },
+        'leave the slow cooker on a low heat': {
+            'markup': 'leave the <mark>slow cooker</mark> on a low heat',
+            'appliances': ['slow cooker'],
+        },
+        'place casserole dish in oven': {
+            'markup': 'place <mark>casserole dish</mark> in <mark>oven</mark>',
+            'appliances': ['oven'],
+            'vessels': ['casserole dish'],
+        },
+        'empty skewer into the karahi': {
+            'markup': 'empty <mark>skewer</mark> into the <mark>karahi</mark>',
+            'utensils': ['skewer'],
+            'vessels': ['karahi'],
+        },
+    }
+
+    response = client.post('/', data={
+        'descriptions[]': list(description_equipment.keys())
+    })
+    for result in response.json:
+        assert result['description'] in description_equipment
+        for key, value in description_equipment[result['description']].items():
+            assert result[key] == value
