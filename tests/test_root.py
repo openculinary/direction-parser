3445a94fb77c3e133b4d28c755c77a6d55b8b1fb
James Addison <james@reciperadar.com>
2020-06-28 19:35:41 +0100

    Migrate direction queries to knowledge-graph service

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,29 +1,7 @@
-def test_description_parsing(client):
+def test_request(client, knowledge_graph_stub):
     description_equipment = {
-        'Pre-heat the oven to 250 degrees F.': {
-            'markup': 'Pre-heat the <mark>oven</mark> to 250 degrees F.',
-            'appliances': [{'appliance': 'oven'}],
-        },
-        'leave the Slow cooker on a low heat': {
-            'markup': 'leave the <mark>Slow cooker</mark> on a low heat',
-            'appliances': [{'appliance': 'slow cooker'}],
-        },
-        'place casserole dish in oven': {
-            'markup': 'place <mark>casserole dish</mark> in <mark>oven</mark>',
-            'appliances': [{'appliance': 'oven'}],
-            'vessels': [{'vessel': 'casserole dish'}],
-        },
-        'empty skewer into the karahi': {
-            'markup': 'empty <mark>skewer</mark> into the <mark>karahi</mark>',
-            'utensils': [{'utensil': 'skewer'}],
-            'vessels': [{'vessel': 'karahi'}],
-        },
     }
-
     response = client.post('/', data={
         'descriptions[]': list(description_equipment.keys())
     })
-    for result in response.json:
-        assert result['description'] in description_equipment
-        for key, value in description_equipment[result['description']].items():
-            assert result[key] == value
+    assert response.status_code == 500
