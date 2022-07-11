71908029103d6caacbcead72caa1fba1c3d2db3b
James Addison <james@reciperadar.com>
2022-07-11 01:10:05 +0100

    Migrate from 'requests' to 'httpx'

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import requests
+import httpx
 
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def root():
     descriptions = request.form.getlist("descriptions[]")
-    equipment_data = requests.post(
+    equipment_data = httpx.post(
         url="http://knowledge-graph-service/directions/query",
         data={"descriptions[]": descriptions},
         proxies={},
