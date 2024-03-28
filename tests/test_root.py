ec0bca71705907a26136404a7e9b7da048b9d76c
James Addison <james@reciperadar.com>
2024-03-28 19:04:45 +0000

    Fixup: relocate pytest.mark.respx decorators from fixtures to test functions.

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.respx(base_url="http://knowledge-graph-service")
 def test_request(client, knowledge_graph_stub):
     description_equipment = {}
     response = client.post(
