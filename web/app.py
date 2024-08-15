f3ef6d66e31a91349f467c77c6e67b0c62a05cc4
James Addison <55152140+jayaddison@users.noreply.github.com>
2024-08-15 15:55:54 +0100

    i18n: add `language_code` argument to parser interface (#14)

--- web/app.py
+++ web/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def root():
+    _ = request.form.get("language_code", type=str, default="en")
     descriptions = request.form.getlist("descriptions[]")
     equipment_data = httpx.post(
         url="http://knowledge-graph-service/directions/query",
