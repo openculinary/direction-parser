a3b15f001ebd095c0443a4969d9326f10876bbfd
James Addison <james@reciperadar.com>
2024-08-15 15:23:08 +0100

    i18n: add `language_code` argument to parser interface

--- tests/test_root.py
+++ tests/test_root.py
@@ -5,6 +5,10 @@ import pytest
 def test_request(client, knowledge_graph_stub):
     description_equipment = {}
     response = client.post(
-        "/", data={"descriptions[]": list(description_equipment.keys())}
+        "/",
+        data={
+            "language_code": "en",
+            "descriptions[]": list(description_equipment.keys()),
+        },
     )
     assert response.status_code == 500
